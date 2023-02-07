872384a86c2a85b507eedd0b0d10f5092cc9af87
zacki744 <zackisjostrom@gmail.com>
2023-02-07 14:46:56 +0100

    .

--- Software_test/test.cpp
+++ Software_test/test.cpp
@@ -3,58 +3,78 @@
 
 
 
-TEST(GameTest, InteractionOptionsInteractWithObj)
+TEST(InteractionOptionsTest, InteractWithObjTest)
 {
-    InteractionOptions inter;
+    InteractionOptions interOpt;
 
-    std::pair<std::string, bool> res_orange_eat = inter.interakt_with_obj("orange", "eat");
-    std::pair<std::string, bool> res_orange_look = inter.interakt_with_obj("orange", "look");
-    std::pair<std::string, bool> res_banana_eat = inter.interakt_with_obj("banana", "eat");
-    std::pair<std::string, bool> res_door_open = inter.interakt_with_obj("door", "open");
-    std::pair<std::string, bool> res_invalid = inter.interakt_with_obj("invalid", "invalid");
+    // Test with valid objects
+    auto result = interOpt.interakt_with_obj("orange", "eat");
+    EXPECT_EQ(result.first, "you become sick from eating the old orange");
+    EXPECT_TRUE(result.second);
 
-    EXPECT_EQ(res_orange_eat.first, "you become sick from eating the old orange");
-    EXPECT_TRUE(res_orange_eat.second);
-    EXPECT_EQ(res_orange_look.first, "you see a lot of mold on the orange");
-    EXPECT_TRUE(res_orange_look.second);
-    EXPECT_EQ(res_banana_eat.first, "you eat the banana and feal invigorated");
-    EXPECT_TRUE(res_banana_eat.second);
-    EXPECT_EQ(res_door_open.first, "You enter the room");
-    EXPECT_TRUE(res_door_open.second);
-    EXPECT_EQ(res_invalid.first, "invalid action");
-    EXPECT_FALSE(res_invalid.second);
+    result = interOpt.interakt_with_obj("banana", "eat");
+    EXPECT_EQ(result.first, "you eat the banana and feal invigorated");
+    EXPECT_TRUE(result.second);
+
+    result = interOpt.interakt_with_obj("door", "open");
+    EXPECT_EQ(result.first, "You enter the room");
+    EXPECT_TRUE(result.second);
+
+    // Test with invalid objects
+    result = interOpt.interakt_with_obj("orange", "kick");
+    EXPECT_EQ(result.first, "invalid action");
+    EXPECT_FALSE(result.second);
+
+    result = interOpt.interakt_with_obj("table", "open");
+    EXPECT_EQ(result.first, "invalid action");
+    EXPECT_FALSE(result.second);
 }
 
-TEST(GameTest, PlayerInv)
-{
-    Player charekter;
-
-    EXPECT_EQ(charekter.Get_inv().size(), 0);
-    charekter.add_to_inv("banana");
-    EXPECT_EQ(charekter.Get_inv().size(), 1);
-    EXPECT_EQ(charekter.get_first(), "banana");
-    charekter.remove_obj();
-    EXPECT_EQ(charekter.Get_inv().size(), 0);
+TEST(GameObjectTest, SetGetObjectTest) {
+    GameObject gameObj;
+    gameObj.set_obj("banana");
+    EXPECT_EQ(gameObj.get_obj(), "banana");
 }
 
-TEST(GameTest, SceneGetCurrentScene)
-{
+TEST(PlayerTest, AddGetObjectTest) {
+    Player player;
+    player.add_to_inv("banana");
+    player.add_to_inv("orange");
+    auto inv = player.Get_inv();
+    EXPECT_EQ(inv[0].get_obj(), "banana");
+    EXPECT_EQ(inv[1].get_obj(), "orange");
+}
+
+TEST(PlayerTest, GetFirstObjectTest) {
+    Player player;
+    player.add_to_inv("banana");
+    player.add_to_inv("orange");
+    EXPECT_EQ(player.get_first(), "banana");
+}
+
+TEST(PlayerTest, RemoveObjectTest) {
+    Player player;
+    player.add_to_inv("banana");
+    player.add_to_inv("orange");
+    player.remove_obj();
+
+    auto inv = player.Get_inv();
+
+    EXPECT_EQ(inv.size(), 1);
+    EXPECT_EQ(inv[0].get_obj(), "banana");
+}
+
+TEST(SceneTest, GetCurrentSceneTest) {
     Scene scene;
-
-    std::pair<std::string, std::string> first = scene.Gett_current_scene();
-    EXPECT_EQ(first.first, "You wake upp in a dimm room and you have a banana in your hand.");
-    EXPECT_EQ(first.second, "banana");
-    scene.next_scene();
-    std::pair<std::string, std::string> second = scene.Gett_current_scene();
-    EXPECT_EQ(second.first, "You notes that there is a door in front of you.");
-    EXPECT_EQ(second.second, "door");
-    scene.next_scene();
-    std::pair<std::string, std::string> third = scene.Gett_current_scene();
-    EXPECT_EQ(third.first, "In the room you find an orange.");
-    EXPECT_EQ(third.second, "orange");
+    auto res = scene.Gett_current_scene();
+    EXPECT_EQ(res.first, "You wake upp in a dimm room and you have a banana in your hand.");
+    EXPECT_EQ(res.second, "banana");
 }
 
-//int main(int argc, char** argv) {
-//    ::testing::InitGoogleTest(&argc, argv);
-//    return RUN_ALL_TESTS();
-//}
+TEST(SceneTest, NextSceneTest) {
+    Scene scene;
+    scene.next_scene();
+    auto res = scene.Gett_current_scene();
+    EXPECT_EQ(res.first, "You notes that there is a door in front of you.");
+    EXPECT_EQ(res.second, "door");
+}
